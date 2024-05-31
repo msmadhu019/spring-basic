695aea41ba258aa87cfd2d11b313a1f5be0c2b2a
msmadhu019 <msmadhu019@gmail.com>
2024-05-31 20:12:59 +0530

    new

---
+++ src/test/java/org/example/springbootbasics/SpringBootBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
